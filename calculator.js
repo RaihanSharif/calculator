d043e102d545a8ac92202ed718bfb1c0bb889710
Raihan Sharif <raihansharif92@gmail.com>
2024-08-11 21:50:47 +0100

    display shows button input as they happen
    display can also be cleared and backspace works

--- calculator.js
+++ calculator.js
@@ -27,3 +27,64 @@ function operate(num1, operator, num2) {
             break;
     }
 }
+
+const calcContainer = document.querySelector('#calc-container');
+const display = document.querySelector('#display');
+calcContainer.addEventListener('click', (event) => {
+    const target = event.target;
+    
+    switch(target.id) {
+        case "backspace":
+            display.textContent = display.textContent.slice(0, -1);
+            break;
+        case "clear":
+            display.textContent = '';
+            break;
+        case "1":
+            display.textContent += 1;
+            break;
+        case "2":
+            display.textContent += 2;
+            break;
+        case "3":
+            display.textContent += 3;
+            break;
+        case "4":
+            display.textContent += 4;
+            break;
+        case "5":
+            display.textContent += 5;
+            break;
+        case "6":
+            display.textContent += 6;
+            break;
+        case "7":
+            display.textContent += 7;
+            break;
+        case "8":
+            display.textContent += 8;
+            break;
+        case "9":
+            display.textContent += 9;
+            break;
+        case "0":
+            display.textContent += 0;
+            break;
+        case "decimal":
+            display.textContent += ".";
+            break;
+        case "plus":
+            display.textContent += "+";
+            break;
+        case "minus":
+            display.textContent += "-";
+            break;
+        case "multiply":
+            display.textContent += "*";
+            break;
+        case "divide":
+            display.textContent += "/";
+            break;
+
+    }
+});
