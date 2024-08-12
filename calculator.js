48911062b7c88b81db56f28f6d4ff064f3e44ea7
Raihan Sharif <raihansharif92@gmail.com>
2024-08-12 11:54:42 +0100

    Plan to rewrite by breaking down into functions

--- calculator.js
+++ calculator.js
@@ -25,18 +25,25 @@ function operate(num1, operator, num2) {
 }
 
 // TODO: add +/- functionality 
+// break the first != '' and first '' into functions
+// break clear, backspace into functions
+// create a set operator function
+// seperate */+- from equals and other operators
+
 const calcContainer = document.querySelector('#calc-container');
 let display = document.querySelector('#display');
-
+let isLastButtonOperator = false;
 calcContainer.addEventListener('click', (event) => {
     const target = event.target;
 
     if (target.classList.contains('numeric')) {
         second += target.value;
         display.textContent = second;
+        isLastButtonOperator = false;
     }
     
     if(target.classList.contains('operator')) {
+        isLastButtonOperator = true;
         if (first != '') {
             first = operate(parseFloat(first), operator, parseFloat(second));
             console.log(`I'm in first if, first: ${first}, second: ${second}.`);
@@ -67,19 +74,33 @@ calcContainer.addEventListener('click', (event) => {
 first = the result of previous operands
 second = always the place for new operands
 
-when press numeric:
+on click press numeric:
     second += button value
     display second
 
-when press operator:
-    if first not empty and operator not empty:
-      first = operate(first, operator, second)
-      dislay first
-      operator = this operator
-      second = empty
+on click math operator:
+    if last press was a math operator
+        first = first
+        operator = operator
+        second = empty
+        /// not sure about that, for now just prevent consequtive presses of operators
+    if first not empty:
+                first = operate(first, operator, second)  // this block should be a seperate function
+                dislay first
+                operator = this operator
+                second = empty
     if first empty:
-      first = second
-      second = empty
-      operator = this operator
-      display first
+                first = second    // this block should be a different function
+                second = empty
+                operator = this operator
+                display first
+
+
+on click = operator:
+    if first or second empty, do nothing
+    else call operate
+    store result in first, clear second
+
+on click backspace operator:
+    update second
 */
